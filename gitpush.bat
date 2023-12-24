5aeca6f6f3713fe241399909f392bc4f0f4f674a
chyhhwen <chyhhwen@gmail.com>
2023-12-24 13:48:42 +0800

    test

--- gitpush.bat
+++ gitpush.bat
@@ -1,6 +1,7 @@
  @echo off
 git config --global user.email "chyhhwen@gmail.com"
 git config --global user.name "chyhhwen"
+git checkout dev
 git add .
 git commit -m "test"
 git push -u origin master
